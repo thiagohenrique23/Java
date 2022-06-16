12e4bbae3b7cda0461c46268b4c4635e85c028ff
thiagohenrique23 <thiagohenrique2309@gmail.com>
2022-06-16 17:01:13 -0300

    Conceitos básicos

---
+++ java/Operador.java
@@ -0,0 +1,16 @@
+public class Operador {
+
+  public static void main(String[] args) {
+    int som = 2 + 8;
+    int sub = 12 - 2;
+    int mul = 2 * 5;
+    int div = 20 / 2;
+    int res = 3 % 2;
+
+    System.out.println(som);
+    System.out.println(sub);
+    System.out.println(mul);
+    System.out.println(div);
+    System.out.println(res);
+  }
+}
